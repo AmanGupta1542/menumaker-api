bd0871a4fa81002e99ebf41eebba049d98ae5636
AmanGupta1542 <amangupta1542@gmail.com>
2024-06-22 18:16:47 +0530

    models added

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', "last_name", 'email', 'password', 'is_staff', 'is_superuser', 'is_active']
+
+
+@admin.register(ResetPasswordToken)
+class ResetPasswordTokenAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'token', 'is_reset', 'created_at', 'updated_at']
+
+
+@admin.register(Countries)
+class CountriesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'code', 'name', 'phone', 'symbol', 'capital', 'currency', 'continent', 'continent_code', 'alpha3', 'created_at']
+
+@admin.register(TokenType)
+class TokenTypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type', 'created_at']
+
+
+@admin.register(UserToken)
+class UserTokenAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'token_count', 'token_type', 'is_used', 'used_token_count', 'created_at']
+
+
+@admin.register(UserCuisine)
+class UserCuisineAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cuisine', 'name', 'user', 'is_completed',  'created_at', 'updated_at']
+
+@admin.register(TokenUsage)
+class TokenUsageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'token', 'cuisine', 'created_at']
+
+@admin.register(MealTimes)
+class MealTimesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'meal_name', 'quote', 'description', 'created_at']
+
+@admin.register(Dishes)
+class DishesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'meal_time', 'country', 'is_active', 'created_at', 'created_by']
+
+@admin.register(CuisineItems)
+class CuisineItemsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'cuisine', 'dish', 'created_at']
+
+@admin.register(DishImages)
+class DishImagesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'dish', 'image', 'alt', 'is_active', 'created_at', 'created_by']
+
+
+@admin.register(DishIngedients)
+class DishIngedientsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'dish', 'ingedients', 'created_at', 'created_by', 'updated_at', 'updated_by']
+
+
+@admin.register(DishRecipe)
+class DishRecipeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'dish', 'recipe', 'created_at', 'created_by', 'updated_at', 'updated_by']
--- api/models.py
+++ api/models.py
@@ -1,3 +1,128 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+from django.utils.translation import gettext_lazy as _
+
+class Countries(models.Model):
+    code = models.CharField(max_length=2)
+    name = models.CharField(max_length=255, unique=True)
+    phone = models.CharField(max_length=15)
+    symbol = models.CharField(max_length=10)
+    capital = models.CharField(max_length=255)
+    currency = models.CharField(max_length=3)
+    continent = models.CharField(max_length=100)
+    continent_code = models.CharField(max_length=2)
+    alpha3 = models.CharField(max_length=3)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    password = models.CharField(max_length=255)
+    country = models.ForeignKey(Countries, on_delete=models.RESTRICT)
+    mobile = models.CharField(max_length=15, unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
+
+class TokenType(models.Model):
+    type = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserToken(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tokens_for_user')
+    token_count = models.IntegerField()
+    token_type = models.ForeignKey(TokenType, on_delete=models.CASCADE, related_name='token_type_for_user')
+    is_used = models.BooleanField(default=0)
+    used_token_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserCuisine(models.Model):
+    cuisine = models.CharField(max_length=60, unique=True)
+    name = models.CharField(max_length=500)
+    user = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, related_name='user_cuisine')
+    is_completed = models.BooleanField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+class TokenUsage(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='token_user')
+    token = models.ForeignKey(UserToken, on_delete=models.CASCADE, related_name='token_uses_from')
+    cuisine = models.ForeignKey(UserCuisine, on_delete=models.CASCADE, related_name='token_used_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'cuisine')
+
+
+class MealTimes(models.Model):
+    meal_name = models.CharField(max_length=100, unique=True)
+    quote = models.CharField(max_length=1000, unique=True)
+    description = models.CharField(max_length=1000, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Dishes(models.Model):
+    name = models.CharField(max_length=255)
+    meal_time = models.ForeignKey(MealTimes, on_delete=models.RESTRICT, related_name='dish_meal_time')
+    country = models.ForeignKey(Countries, on_delete=models.RESTRICT, related_name='dish_country')
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, related_name='dish_added_by')
+
+    
+class CuisineItems(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, related_name='cuisine_items_for_user')
+    cuisine = models.ForeignKey(UserCuisine, on_delete=models.RESTRICT, related_name='belong_to_cuisine')
+    dish = models.ForeignKey(Dishes, on_delete=models.RESTRICT, related_name='for_dish')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'cuisine', 'dish')
+
+
+class DishImages(models.Model):
+    dish = models.ForeignKey(Dishes, on_delete=models.RESTRICT, related_name='img_for_dish')
+    image = models.ImageField(upload_to='dish_images')
+    alt = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, related_name='dish_img_add_by')
+
+
+class DishIngedients(models.Model):
+    dish = models.OneToOneField(Dishes, on_delete=models.RESTRICT, related_name='ingedients_for_dish')
+    ingedients = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, related_name='ingedients_add_by')
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, related_name='ingedients_update_by', null=True, blank=True)
+
+
+class DishRecipe(models.Model):
+    dish = models.OneToOneField(Dishes, on_delete=models.RESTRICT, related_name='recipe_for_dish')
+    recipe = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, related_name='recipe_add_by')
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, related_name='recipe_update_by', null=True, blank=True)
+
+
+class ResetPasswordToken(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, related_name='reset_token_for_user')
+    token = models.CharField(max_length=1000)
+    is_reset = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
